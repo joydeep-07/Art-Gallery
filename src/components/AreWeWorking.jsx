470a11852d3127e9df2734dde701f4211cd22aaa
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-30 23:20:37 +0530

    Updated Design

--- src/components/AreWeWorking.jsx
+++ src/components/AreWeWorking.jsx
@@ -70,44 +70,70 @@ const AreWeWorking = () => {
               process. I've led design teams, building bridges between
               stakeholders, users, and team members.
             </h2>
-            <img className="h-110 flex-shrink-0" src={img} alt="art" />
+            <div className="p-3 border">
+              <img
+                className="h-110 p-3 border flex-shrink-0"
+                src={img}
+                alt="art"
+              />
+            </div>
           </div>
 
           <div className="flex-shrink-0 w-screen flex justify-center items-center gap-16 px-16 pb-70">
             <h2
               style={{ fontFamily: "karatone" }}
-              className="text-neutral-700 text-5xl max-w-3xl flex-shrink-0 whitespace-normal text-center"
+              className="text-neutral-700 text-left text-4xl max-w-3xl flex-shrink-0 whitespace-normal "
             >
-              My focus is on building digital experiences that feel natural,
-              intuitive, and visually balanced. I believe design is not just
-              about aesthetics — it’s about creating meaning through form and
-              function.
+              I've been designing for 17 years. My background in art and
+              technology allows me to flexibly manage the product development
+              process. I've led design teams, building bridges between
+              stakeholders, users, and team members.
             </h2>
-            <img className="h-110 flex-shrink-0" src={img} alt="art" />
+            <div className="p-3 border">
+              <img
+                className="h-110 p-3 border flex-shrink-0"
+                src={img}
+                alt="art"
+              />
+            </div>
           </div>
 
           <div className="flex-shrink-0 w-screen flex justify-center items-center gap-16 px-16 pb-70">
             <h2
               style={{ fontFamily: "karatone" }}
-              className="text-neutral-700 text-5xl max-w-3xl flex-shrink-0 whitespace-normal text-center"
+              className="text-neutral-700 text-left text-4xl max-w-3xl flex-shrink-0 whitespace-normal "
             >
-              Over the years, I’ve collaborated with global brands and startups
-              to create memorable product experiences. Every project begins with
-              empathy — understanding the human at the center of it all.
+              I've been designing for 17 years. My background in art and
+              technology allows me to flexibly manage the product development
+              process. I've led design teams, building bridges between
+              stakeholders, users, and team members.
             </h2>
-            <img className="h-110 flex-shrink-0" src={img} alt="art" />
+            <div className="p-3 border">
+              <img
+                className="h-110 p-3 border flex-shrink-0"
+                src={img}
+                alt="art"
+              />
+            </div>
           </div>
 
           <div className="flex-shrink-0 w-screen flex justify-center items-center gap-16 px-16 pb-70">
             <h2
               style={{ fontFamily: "karatone" }}
-              className="text-neutral-700 text-5xl max-w-3xl flex-shrink-0 whitespace-normal text-center"
+              className="text-neutral-700 text-left text-4xl max-w-3xl flex-shrink-0 whitespace-normal "
             >
-              My creative journey continues to evolve with every idea, every
-              sketch, and every iteration. Design, for me, is an endless
-              conversation between curiosity and creation.
+              I've been designing for 17 years. My background in art and
+              technology allows me to flexibly manage the product development
+              process. I've led design teams, building bridges between
+              stakeholders, users, and team members.
             </h2>
-            <img className="h-110 flex-shrink-0" src={img} alt="art" />
+            <div className="p-3 border">
+              <img
+                className="h-110 p-3 border flex-shrink-0"
+                src={img}
+                alt="art"
+              />
+            </div>
           </div>
         </div>
 
@@ -115,14 +141,14 @@ const AreWeWorking = () => {
         <div className="absolute bottom-5 left-0 w-full z-50 overflow-hidden">
           <div ref={mainTextContentRef} className="flex whitespace-nowrap">
             <h1
-            style={{
-                fontFamily:"karatone",
-                lineHeight:1,
-            }}
-            className="text-[150px] font-bold text-neutral-800 inline-block ml-[100px] mr-20">
-             We create Experience that tells a Story
+              style={{
+                fontFamily: "karatone",
+                lineHeight: 1,
+              }}
+              className="text-[150px] font-bold text-neutral-800 inline-block ml-[100px] mr-20"
+            >
+              We create Experience that tells a Story
             </h1>
-            
           </div>
         </div>
       </section>
