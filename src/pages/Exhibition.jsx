bc840a18cafb48b1a9ebc9b8b2440e5ccf2d961a
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-11-18 16:09:07 +0530

    Updated

--- src/pages/Exhibition.jsx
+++ src/pages/Exhibition.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../layouts/Footer";
-
+import Gallery from "../pages/Gallery";
 const Exhibition = () => {
   return (
     <div className="min-h-screen flex flex-col">
       {/* PAGE CONTENT */}
-      <div className=" flex-grow text-5xl text-center">Exhibition</div>
+      <div className=" flex-grow text-5xl text-center">
+        <Gallery/>
+      </div>
 
       {/* FOOTER STAYS AT BOTTOM */}
       <Footer />
