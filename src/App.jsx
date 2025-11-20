0fe3b81e9aa7cfd8f5be34258fc5fa0ab8584ea5
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-11-20 18:12:20 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       const Lenis = LenisModule.default;
 
       const lenis = new Lenis({
-        duration: 1.2,
+        duration: 0.8,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         direction: "vertical",
         gestureDirection: "vertical",
